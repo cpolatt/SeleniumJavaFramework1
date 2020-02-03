99c95b9f1ab3d248954e73cb9cba6d15e6d78149
cpolat <cpolat@cpolat.bimser.local>
2020-02-03 10:11:25 +0300

    First Commit - Added project

---
+++ SeleniumFramework/src/test/java/BrowserTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BrowserTest {
+	
+	public static void main(String[] args) {
+		
+		String projectPath = System.getProperty("user.dir");
+	    System.out.println(projectPath);
+	    
+	    System.setProperty("webdriver.gecko.driver", projectPath + "/drivers/geckodriver/geckodriver.exe");
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://google.com");
+		
+		driver.close();
+		driver.quit();
+	}
+
+
+}
